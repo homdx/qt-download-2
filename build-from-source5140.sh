565ddc3aedc11c47889fd357c896ee803de15513
HomDX <rgaraev@gmcs.ru>
2019-11-05 17:18:11 +0300

    added env for build

--- build-from-source5140.sh
+++ build-from-source5140.sh
@@ -22,6 +22,7 @@ export    ANDROID_NDK_EABI=llvm c && \
 export    ANDROID_NDK_HOST=linux-x86_64 c && \
 export    ANDROID_NDK_TOOLCHAIN_PREFIX=arm-linux-androideabi c && \
 export    ANDROID_NDK_TOOLCHAIN_VERSION=4.9 c && \
+export    ANDROID_BUILD_TOOLS_REVISION=29.0.5 && \
 export DEBIAN_FRONTEND=noninteractive c && \
 cd /Qt5140/Qt/5.14.0/Src && echo start build && date && LANG=C ./configure -android-arch armeabi-v7a -opensource -confirm-license -release -nomake tests -nomake examples -no-compile-examples -android-sdk /android-sdk-linux -android-ndk /android-ndk-r20 -xplatform android-clang -no-warnings-are-errors --disable-rpath \
 -openssl -I /android_openssl/openssl-1.1.1d/include -L /android_openssl/arm \

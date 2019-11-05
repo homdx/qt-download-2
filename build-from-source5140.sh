2573af5abaa6feb2a5911dd949051c2a09fde829
homdx <37062532+homdx@users.noreply.github.com>
2019-11-05 11:04:10 +0300

    script build fix1

--- build-from-source5140.sh
+++ build-from-source5140.sh
@@ -9,16 +9,13 @@ export QT_VERSION=5.14.0
 echo QT version is $QT_VERSION
 echo show envs in build script
 set
-export PATH="/Qt5140/Qt/$QT_VERSION/android_armv7/bin/:${PATH}"
+export PATH="/Qt5140/Qt/$QT_VERSION/android_armv7/bin:/Qt5140/Qt/$QT_VERSION/bin:${PATH}"
 echo show new envs in build script
 set
 export QT_HOME=/Qt5140/Qt/$QT_VERSION/
 echo search whereis
 whereis qmake
 
-apt install build-essential g++ -y && \
-apt-get install gcc git bison python gperf pkg-config gdb-multiarch -y && \
-apt-get install libgles2-mesa-dev -y && \
 export NDK_VERSION=r20 && \
 export    ANDROID_NDK_ARCH=arch-arm c && \
 export    ANDROID_NDK_EABI=llvm c && \
@@ -27,5 +24,5 @@ export    ANDROID_NDK_TOOLCHAIN_PREFIX=arm-linux-androideabi c && \
 export    ANDROID_NDK_TOOLCHAIN_VERSION=4.9 c && \
 export DEBIAN_FRONTEND=noninteractive c && \
 cd /Qt5140/Qt/5.14.0/Src && echo start build && date && LANG=C ./configure -android-arch armeabi-v7a -opensource -confirm-license -release -nomake tests -nomake examples -no-compile-examples -android-sdk /android-sdk-linux -android-ndk /android-ndk-r20 -xplatform android-clang -no-warnings-are-errors --disable-rpath \
--openssl -I /android_openssl/openssl-1.1.1d/include -L /android_openssl/arm && \
+-openssl -I /android_openssl/openssl-1.1.1d/include -L /android_openssl/arm \
 && make --no-print-directory && echo end build && date && echo build done && make install && cd /Qt5140/Qt/5.14.0/Src/qtbase/src/tools/androiddeployqt && make && make install &&  echo done1 && date && echo rm -rf /Qt && date && echo all done ok || echo error build
